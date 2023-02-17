3e93925479b7631edea136660f512a82d2babb1e
Ondřej Kováč <96345636+ondrejkovac021@users.noreply.github.com>
2023-02-17 17:35:27 +0100

    Update and rename PIP.py to PIP

--- PIP.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-def install_package(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-install_package("") # do zavorak zadat jmeno balíčku např. NumPy
